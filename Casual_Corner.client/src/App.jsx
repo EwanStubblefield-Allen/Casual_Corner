67f2fd34336873a0356cdc115bb59e7d25ce0963
EwanStubblefield-Allen <ewan2004@yahoo.com>
2023-11-16 14:45:54 -0700

    first commit

---
+++ Casual_Corner.client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import { Navbar } from './components/Navbar.jsx'
+
+export function App() {
+  return (
+    <div className="App" id="app">
+      <header>
+        <Navbar />
+      </header>
+
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}
---
+++ Casual_Corner.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'https://localhost:7045' : ''
+export const useSockets = false
+export const domain = 'dev-4sjhjftaw74ytohu.us.auth0.com'
+export const clientId = '1IsKgLLy06BzrKd64KnDD2KSWDzEfuJs'
+export const audience = 'classroom.com'
