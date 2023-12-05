c1627d2eb243d802e9a489d39558b36299f9e707
EwanStubblefield-Allen <ewan2004@yahoo.com>
2023-12-05 15:22:21 -0700

    Getting userInfo

--- Casual_Corner/src/main/java/com/Casual_Corner/Controllers/AccountController.java
+++ Casual_Corner/src/main/java/com/Casual_Corner/Controllers/AccountController.java
@@ -1,32 +1,37 @@
 package com.Casual_Corner.Controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Casual_Corner.SecurityConfig;
 import com.Casual_Corner.Models.Account;
 import com.Casual_Corner.Services.AccountService;
+
 import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("account")
 public class AccountController {
   private final AccountService accountService;
+  private final SecurityConfig securityConfig;
 
   @Autowired
-  public AccountController(AccountService accountService) {
+  public AccountController(AccountService accountService, SecurityConfig securityConfig) {
     this.accountService = accountService;
+    this.securityConfig = securityConfig;
   }
 
   @GetMapping
-  public Object get() {
-    return accountService.getOrCreateProfile();
+  public Account get(@RequestHeader(name = "Authorization") String token) {
+    Account userInfo = securityConfig.getUserInfo(token);
+    return accountService.getOrCreateProfile(userInfo);
   }
 
   @PutMapping
--- Casual_Corner/src/main/java/com/Casual_Corner/SecurityConfig.java
+++ Casual_Corner/src/main/java/com/Casual_Corner/SecurityConfig.java
@@ -2,13 +2,26 @@ package com.Casual_Corner;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.client.RestTemplate;
+
+import com.Casual_Corner.Models.Account;
 
 @Configuration
 public class SecurityConfig {
+  private final Environment env;
+
+  public SecurityConfig(Environment env) {
+    this.env = env;
+  }
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     return http
@@ -21,5 +34,18 @@ public class SecurityConfig {
         .cors(Customizer.withDefaults())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
         .build();
+  }
+
+  public Account getUserInfo(String token) {
+    String url = env.getProperty("okta.oauth2.issuer") + "userinfo";
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.set("Authorization", token);
+    HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+
+    RestTemplate restTemplate = new RestTemplate();
+    ResponseEntity<Account> response = restTemplate.exchange(url, HttpMethod.GET, entity, Account.class);
+
+    return response.getBody();
   }
 }
